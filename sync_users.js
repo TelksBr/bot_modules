fb33021e6bec0697e8c41dc97e24fa7b49244df1
Vinicius Rodrigues <vinizika508@gmail.com>
2023-10-30 23:59:49 -0300

    Update sync_users.js

--- sync_users.js
+++ sync_users.js
@@ -9,7 +9,7 @@ async function runCommand(user, position) {
 
     const token = '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';
     const url = 'https://raw.githubusercontent.com/TelksBr/bot_modules/main/create_user.sh';
-    const command = `rm -r create_user.sh && rm -r users.json* && wget http://bot.sshtproject.com/backup/users.json && wget --header="Authorization: Bearer ${token}" ${url} && chmod +x create_user.sh && ./create_user.sh ${username} ${password} ${validate} ${limite}`;
+    const command = `./create_user.sh ${username} ${password} ${validate} ${limite}`;
     
 
     try {
