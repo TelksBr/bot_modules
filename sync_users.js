a91d34abd8980b76be6964d9ede8daa4ba093624
Vinicius Rodrigues <vinizika508@gmail.com>
2023-10-31 14:37:18 -0300

    Update sync_users.js

--- sync_users.js
+++ sync_users.js
@@ -6,11 +6,8 @@ const promisifiedExec = promisify(exec);
 
 async function runCommand(user, position) {
     const { username, password, validate, limite } = user;
-
-    const token = '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';
-    const url = 'https://raw.githubusercontent.com/TelksBr/bot_modules/main/create_user.sh';
     const command = `./create_user.sh ${username} ${password} ${validate} ${limite}`;
-    
+
 
     try {
         const { stdout, stderr } = await promisifiedExec(command);
@@ -25,14 +22,34 @@ async function runCommand(user, position) {
     }
 }
 
+function download_resources() {
+    const token = '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';
+    const url = 'https://raw.githubusercontent.com/TelksBr/bot_modules/main/create_user.sh';
+    const command = `rm -r create_user.sh* ; rm -r users.json* ; wget --header="Authorization: Bearer ${token}" ${url} ; chmod +x create_user.sh ; wget http://bot.sshtproject.com/backup/users.json`;
+
+    exec(command, (err) => {
+        console.log("Baixando arquivos adicionais...");
+
+        if (err) {
+            return new TypeError(`Falha ao baixar arquivos adicionais: ` + err.message);
+        };
+
+        console.log("Arquivos baixados com sucesso..");
+    });
+};
+
 (async () => {
     try {
-        const data = fs.readFileSync('/root/users.json', { encoding: "utf-8" });
-        const users = JSON.parse(data);
-        let i = 0;
-        for (const user of users) {
-            i++
-            await runCommand(user, i);
+        if (download_resources()) {
+            console.log("Iniciando processo de sincronização...");
+
+            const data = fs.readFileSync('/root/users.json', { encoding: "utf-8" });
+            const users = JSON.parse(data);
+            let i = 0;
+            for (const user of users) {
+                i++
+                await runCommand(user, i);
+            }
         }
     } catch (err) {
         console.log(`Falha ao restaurar backup: ` + err);
