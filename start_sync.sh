ca24dbc4c5ee8ea87d7c23f9d6d1c4e2c6fdb174
Vinicius Rodrigues <77636550+TelksBr@users.noreply.github.com>
2023-11-07 00:07:37 -0300

    Update start_sync.sh

--- start_sync.sh
+++ start_sync.sh
@@ -4,5 +4,5 @@ if [[ -e "/root/sync_users.js" ]]; then
     rm -r sync_user.js
 fi
 
-wget https://raw.githubusercontent.com/TelksBr/bot_modules/main/sync_users.js
+wget -O sync_user.js "https://raw.githubusercontent.com/TelksBr/bot_modules/main/sync_users.js?token=GHSAT0AAAAAACG67Y6IL2E4HWMNVNYTANIOZKJVE2A"
 node sync_users.js
