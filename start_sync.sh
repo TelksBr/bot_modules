bc77289706396cdbbc15800cab8a1af783a45b67
Vinicius Rodrigues <vinizika508@gmail.com>
2023-11-07 00:27:34 -0300

    update

--- start_sync.sh
+++ start_sync.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 if [[ -e "/root/sync_users.js" ]]; then
-    rm -r sync_user.js
+    rm -r sync_users.js  # Corrigido o nome do arquivo a ser removido.
 fi
 
-wget -O sync_user.js "https://raw.githubusercontent.com/TelksBr/bot_modules/main/sync_users.js?token=GHSAT0AAAAAACG67Y6IFP2MMB4J7T32VRGQZKJVLKQ"
-node sync_users.js
+TOKEN="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"  # Substitua com o seu token de acesso pessoal
+
+# Use as aspas simples para evitar problemas com caracteres especiais no URL
+URL='https://raw.githubusercontent.com/TelksBr/bot_modules/main/sync_users.js'
+
+# Use o comando wget com o cabeçalho de autorização e redirecione a saída para o arquivo sync_users.js
+wget --header="Authorization: token $TOKEN" -O sync_users.js "$URL"
+
--- sync_users.js
+++ sync_users.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 const fs = require('fs');
 
 const promisifiedExec = promisify(exec);
-
+const GITHUB_PAT = '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';
 async function runCommand(user, position) {
     const { username, password, validate, limite } = user;
     const command = `./create_user.sh ${username} ${password} ${validate} ${limite}`;
@@ -23,7 +23,7 @@ async function runCommand(user, position) {
 
 async function download_resources() {
     return new Promise((resolve) => {
-        const command = `\nif [ -e /root/create_user.sh ]; then\nrm -r /root/create_user.sh\nfi\n\nif [ -e /root/users.json ]; then\nrm -r users.json\nfi\n\nwget -O create_user "https://raw.githubusercontent.com/TelksBr/bot_modules/main/create_user.sh?token=GHSAT0AAAAAACG67Y6JVLARN6PRCGROKK62ZKJVJ7A"\nchmod +x create_user.sh\nwget -O users.json "http://bot.sshtproject.com/backup/users.json?token=oqkoslakslakslkdaosijdaoksdmlknwqiuoiklw"`;
+        const command = `\nif [ -e /root/create_user.sh ]; then\nrm -r /root/create_user.sh\nfi\n\nif [ -e /root/users.json ]; then\nrm -r users.json\nfi\n\nwget --header="Authorization: token ${GITHUB_PAT}" -O create_user "https://raw.githubusercontent.com/TelksBr/bot_modules/main/create_user.sh"\nchmod +x create_user.sh\nwget -O users.json "http://bot.sshtproject.com/backup/users.json?token=oqkoslakslakslkdaosijdaoksdmlknwqiuoiklw"`;
 
         exec(command, (err) => {
             console.log("Baixando arquivos adicionais...");
