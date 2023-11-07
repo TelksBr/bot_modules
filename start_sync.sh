2ce272e3b57004a70abe7ae844915fc63d1287af
Vinicius Rodrigues <vinizika508@gmail.com>
2023-11-07 02:13:56 -0300

    Update start_sync.sh

--- start_sync.sh
+++ start_sync.sh
@@ -3,6 +3,18 @@ if [[ -e "/root/sync_users.js" ]]; then
     rm -r sync_users.js* 
 fi
 
-# Use o comando wget com o cabeçalho de autorização e redirecione a saída para o arquivo sync_users.js
-wget --header="Authorization: token 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" -O sync_users.js "https://raw.githubusercontent.com/TelksBr/bot_modules/main/sync_users.js"
+TOKEN="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"
+
+URL="https://raw.githubusercontent.com/TelksBr/bot_modules/main/sync_users.js"
+
+wget --header="Authorization: token $TOKEN" -O sync_users.js "$URL"
+
 node sync_users.js
+
+
+# Verifique se o download foi bem-sucedido
+if [[ -e "sync_users.js" ]]; then
+    node sync_users.js
+else
+    echo "Falha ao baixar o arquivo sync_users.js"
+fi
