31afd44f21fd8fadbcdd7ebb51f3da63ede9ec68
Vinicius Rodrigues <77636550+TelksBr@users.noreply.github.com>
2024-02-17 04:00:03 -0300

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,15 @@
+const file = "/etc/v2ray/config.json";
+const index = Number(process?.argv[2]);
+const uuid = process?.argv[3];
+
+const file_data = require("fs").readFileSync(file, "utf-8");
+
+const json = JSON.parse(file_data);
+
+const search = json[index]?.find(obj => obj?.id == uuid);
+
+if (search) {
+    return console.log(true);
+};
+
+return false;
